858f6711f68d45335ab346d10d06b05877a4958c
Brandon Kentel <bkentel@users.noreply.github.com>
2015-06-28 00:09:11 +0900

    clang + catch diagnostic noise

--- test/json_test.cpp
+++ test/json_test.cpp
@@ -1,4 +1,9 @@
 #ifndef BK_NO_UNIT_TESTS
+#include <boost/predef.h>
+#if BOOST_COMP_CLANG
+#   pragma clang diagnostic ignored "-Wexit-time-destructors"
+#endif
+
 #include <catch/catch.hpp>
 
 #include "json_util.hpp"
