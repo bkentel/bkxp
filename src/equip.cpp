12f36f859d4652f382482e871ddb7ea3a89f486c
Brandon Kentel <bkentel@users.noreply.github.com>
2015-08-08 02:22:22 +0900

    add some color and rudimentary grammar functions
    make make_string more optimization friendly
    
    crude grammatical functions

--- src/equip.cpp
+++ src/equip.cpp
@@ -37,12 +37,66 @@ bkrl::equipment::equipment()
 }
 
 //--------------------------------------------------------------------------------------------------
-bkrl::equipment::slot_t const&
+bool bkrl::equipment::is_equipped(item const& i) const noexcept
+{
+    auto islots = i.slots();
+    for (auto const& slot : slots_) {
+        if (islots.none()) {
+            break;
+        }
+
+        if (slot.itm != &i) {
+            continue;
+        }
+
+        islots.clear(slot.type);
+
+        if ( (slot.type == equip_slot::hand_main || slot.type == equip_slot::hand_off)
+          && (islots.test(equip_slot::hand_any))
+        ) {
+            islots.clear(equip_slot::hand_any);
+        }
+    }
+
+    BK_PRECONDITION((islots.any() && islots == i.slots()) || islots.none());
+    return islots.none();
+}
+
+//--------------------------------------------------------------------------------------------------
+bool bkrl::equipment::is_equipped(equip_slot const es) const noexcept
+{
+    if (auto const slot = find_slot_(es)) {
+        return !!slot->itm;
+    }
+
+    return false;
+}
+
+//--------------------------------------------------------------------------------------------------
+bkrl::equipment::slot_t const*
 bkrl::equipment::slot_info(equip_slot const es) const
 {
-    auto const slot = find_slot_(es);
-    BK_PRECONDITION(slot);
-    return *slot;
+    return find_slot_(es);
+}
+
+//--------------------------------------------------------------------------------------------------
+bkrl::item_slots bkrl::equipment::where(item const& i) const noexcept
+{
+    using es = equip_slot;
+
+    item_slots result {};
+
+    for (auto const& slot : slots_) {
+        if (slot.itm == &i) {
+            BK_PRECONDITION((slot.type == es::hand_main || slot.type == equip_slot::hand_off)
+              ? i.slots().any_of({es::hand_any, es::hand_main, es::hand_off})
+              : i.slots().test(slot.type));
+
+            result.set(slot.type);
+        }
+    }
+
+    return result;
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -53,13 +107,20 @@ bkrl::equipment::can_equip(item const & i) const
         return {result_t::status_t::not_equippable};
     }
 
+    if (i.flags().test(item_flag::is_equipped)) {
+        return {result_t::status_t::already_equipped};
+    }
+
     return {result_t::status_t::ok};
 }
 
 //--------------------------------------------------------------------------------------------------
 bkrl::equipment::result_t
-bkrl::equipment::equip(item & i)
+bkrl::equipment::equip(item& i)
 {
+    //
+    // first check whether this is something that can actually be equipped.
+    //
     {
         auto const result = can_equip(i);
         if (!result) {
@@ -67,39 +128,69 @@ bkrl::equipment::equip(item & i)
         }
     }
 
-    auto const slots = i.slots();
-    auto required_slots = slots;
-    auto occupied_slots = item_slots {};
+    auto const islots = i.slots();
+    using es = equip_slot;
 
-    for (auto const& slot : slots_) {
-        if (slot.itm && required_slots.test(slot.type)) {
-            occupied_slots.set(slot.type);
+    //
+    // next check whether the slot requirements can be met
+    //
+    {
+        auto required_slots = islots;
+        auto occupied_slots = item_slots {};
+
+        for (auto const& slot : slots_) {
+            if (slot.itm && required_slots.test(slot.type)) {
+                occupied_slots.set(slot.type);
+            }
+
+            required_slots.clear(slot.type);
+
+            if ( (required_slots.test(es::hand_any))
+              && (slot.type == es::hand_main || slot.type == es::hand_off)
+            ) {
+                required_slots.clear(es::hand_any);
+            }
         }
 
-        required_slots.clear(slot.type);
+        if (occupied_slots.any()) {
+            return make_result(result_t::status_t::slot_occupied, occupied_slots);
+        }
+
+        if (required_slots.any()) {
+            return make_result(result_t::status_t::slot_not_present, required_slots);
+        }
     }
 
-    if (occupied_slots.any()) {
-        return make_result(result_t::status_t::slot_occupied, occupied_slots);
-    }
-
-    if (required_slots.any()) {
-        return make_result(result_t::status_t::slot_not_present, required_slots);
-    }
-
+    auto slots = islots;
     for (auto& slot : slots_) {
-        if (!slots.test(slot.type)) {
+        if (slots.none()) {
+            break;
+        }
+
+        if ( (slots.test(es::hand_any))
+          && (slot.type == es::hand_main || slot.type == es::hand_off)
+          && (slots.test(es::hand_any))
+          && (!slot.itm)
+        ) {
+            slots.clear(es::hand_any);
+        } else if (!slots.test(slot.type)) {
             continue;
+        } else {
+            slots.clear(slot.type);
         }
 
         BK_ASSERT(!slot.itm);
         slot.itm = &i;
     }
 
+    if (slots.any()) {
+        return make_result(result_t::status_t::slot_occupied, slots);
+    }
+
     BK_ASSERT(!i.flags().test(item_flag::is_equipped));
     i.flags().set(item_flag::is_equipped);
 
-    return make_result(result_t::status_t::ok, slots);
+    return make_result(result_t::status_t::ok, islots);
 }
 
 //--------------------------------------------------------------------------------------------------
